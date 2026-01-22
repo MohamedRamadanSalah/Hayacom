a05f393587a795ac9fd920885e55fb73a7641ce7
Mohamed Ramadan <mhmdrmdanslahslah3@gmail.com>
2026-01-22 20:17:06 +0200

    Configration in Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,39 @@
-void main(){
-  
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => CounterProvider())],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Counter App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Container(),
+    );
+  }
+}
+
+class CounterProvider extends ChangeNotifier {
+  int _count = 0;
+
+  int get count => _count;
+
+  void increment() {
+    _count++;
+    notifyListeners();
+  }
+
+  void decrement() {
+    _count--;
+    notifyListeners();
+  }
 }
